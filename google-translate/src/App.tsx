4a1568b4acfced9abc25524a7a9c29a509767c76
Oscar Alva <oscaraldrin23@gmail.com>
2023-04-06 19:34:07 +0200

    hooks

--- google-translate/src/App.tsx
+++ google-translate/src/App.tsx
@@ -1,85 +1,20 @@
-import { useReducer } from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {type Action, type State } from './types.d';
+import { useStore } from './hooks/useStore';
+import './App.css';
 
 
-//1.Create a initial state
-const initialState: State = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false   
-}
-
-//2. Create a reducer 
-function reducer(state: State, action: Action){
-  const {type} = action
-
-  if(type === 'INTERCHANGE_LANGUAGES'){
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    } 
-  }
-  //para cambiar idioma de origen
-  if(type === 'SET_FROM_LANGUAGE'){
-    return{ 
-      ...state,
-      fromLanguage: action.payload
-    }
-  }
-
-  //para cambiar idioma de destino
-  if(type === 'SET_TO_LANGUAGE'){
-    return{
-      ...state,
-      toLanguage: action.payload
-    }
-  }
-
-  if(type === 'SET_FROM_TEXT'){
-    return{
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: ''
-    }
-  }
-
-  if(type === 'SET_RESULT'){
-    return{
-      ...state,
-      loading: false, 
-      result: action.payload
-    }
-  }
-
-  //si no hay ningun tipo devolver el stado
-  return state
-}
 
 function App()  {
-  //3. Usar el hook useReducer
-  const [{
-    fromLanguage,
-    toLanguage,
-    fromText,
-    result,
-    loading
-  }, dispatch] = useReducer(reducer, initialState)
+  const {fromLanguage, setFromLanguage} = useStore();
 
-  console.log({fromLanguage})
 
   return (
     <div className="App">
       <h1>Google Translate</h1>
       <button onClick={() =>{
-        dispatch({type: 'SET_FROM_LANGUAGE', payload: 'es'})
-      }}>Cambiar a Español</button>
-     
+        setFromLanguage('es')//establecer desde el idioma
+      }}>Cambiar a Español</button> 
+      {fromLanguage}
     </div>
   )
 }
