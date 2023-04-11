8e23f9a636dfd5e4b12e3f41d31b8c6acb067dfc
Oscar Alva <oscaraldrin23@gmail.com>
2023-04-11 17:09:20 +0200

    errase .env

--- google-translate/src/services/translate.ts
+++ google-translate/src/services/translate.ts
@@ -1,3 +1,7 @@
 import { OpenAIApi , Configuration} from "openai"
+import { SUPPORTED_LANGUAGES } from "../constants";
 
 const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+const configuration = new Configuration({apiKey});
+
